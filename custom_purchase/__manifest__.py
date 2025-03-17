b69133d47e0779c3dcc9e8c59de56d64d44c5254
kapil <kapil.pysquad@gmail.com>
2025-03-17 16:58:19 +0530

    [Added] New dashbord feature for purchase order

--- custom_purchase/__manifest__.py
+++ custom_purchase/__manifest__.py
@@ -5,10 +5,18 @@
     'website': '',
     'category': 'Purchase',
     'license': 'LGPL-3',
-    'depends': ['purchase'],
+    'depends': ['base', 'purchase', 'hr', 'sign', 'documents'],
     'data': [
+        'security/ir.model.access.csv',
         'views/purchase_order_view.xml',
+        'wizard/purchase_order_sign_wizard.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'custom_purchase/static/src/xml/**',
+            # 'custom_purchase/static/src/js/**',
+        ],
+    },
     'installable': True,
     'application': True,
 }
--- custom_purchase/models/purchase_order.py
+++ custom_purchase/models/purchase_order.py
@@ -1,12 +1,74 @@
 # -*- coding: utf-8 -*-
 
-
 from odoo import models, fields, api, _
+
 
 class InheritPurchaseOrder(models.Model):
     _inherit = 'purchase.order'
     _description = 'Inherit Purchase Order'
 
+    sign_template_ids = fields.One2many('sign.template', 'purchase_order_id')
+    # all_templates_signed = fields.Boolean(compute='_compute_all_templates_signed')
+
+    login_user_manager = fields.Many2one('hr.employee', string='Login User')  # compute = "_is_compute_manager"
+    is_manager = fields.Boolean(string="Is Manager", compute='_is_manager')
+
+
+    def open_sign_document_wizard(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _("SIGN PO"),
+            'res_model': 'purchase.order.sign.wizard',
+            'target': 'new',
+            'view_mode': 'form',
+            # 'domain': [('id', 'in', self.tax_cash_basis_created_move_ids.ids)],
+            # 'views': [(self.env.ref('account.view_move_tree').id, 'tree'), (False, 'form')],
+        }
 
     def purchase_list_view_button(self):
+        '''Added button on list view of purchase order
+            Visible: on selecting record from list view'''
         print('purchase_list_view_button>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+    def create(self, vals):
+        res = super().create(vals)
+        res.login_user_manager = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)],
+                                                                limit=1).parent_id.id
+        return res
+
+    def write(self, vals):
+        vals['login_user_manager'] = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)],
+                                                                    limit=1).parent_id.id
+        return super().write(vals)
+
+    def _is_manager(self):
+        for rec in self:
+            login_user_manager_id = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)])
+            if rec.login_user_manager == login_user_manager_id:
+                rec.is_manager = True
+            else:
+                rec.is_manager = False
+
+    @api.model
+    def retrieve_dashboard(self):
+        res = super(InheritPurchaseOrder, self).retrieve_dashboard()
+        print('Custom retrieve_dashboard>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+        po = self.env['purchase.order']
+        res['all_cancel'] = po.search_count([('state', '=', 'cancel')])
+        res['my_cancel'] = po.search_count([('state', '=', 'cancel'), ('user_id', '=', self.env.uid)])
+
+        return res
+
+    # def _is_compute_manager(self):
+    #     # tried with compute method but this does not store the value or it not working as per expectations
+    #     user_id = self.env.user
+    #     # print('user_id.employee_id>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>',user_id.employee_id)
+    #     for rec in self:
+    #         print('rec..........................',rec.login_user_manager)
+    #         employee_id = rec.env['hr.employee'].search([('user_id', '=', user_id.id)])
+    #         print('manager_id...............................',employee_id.parent_id)
+    #         rec.login_user_manager = employee_id.parent_id.id
+
+    '''login_user_manager : On create or update recorde(PO) this filed will store the manager of login employee
+    is_manager : If the login employee is the manager of PO created employee then it will return true'''
